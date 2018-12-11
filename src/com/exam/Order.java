68deeff32efd76a8abe69d1b938aeca8d3a210f8
Jason <weng8@LAPTOP-G5JC7M00>
2018-12-11 22:33:20 +0800

    Finished

--- src/com/exam/Order.java
+++ src/com/exam/Order.java
@@ -1,5 +1,24 @@
 package com.exam;
 
 public class Order {
+	
+	int i;
+	Ticket[] t;
+	
+	public Order() {
+		i = 0;
+		t = new Ticket[10];
+	}
+	
+	public void add(Ticket ticket) {
+		t[i] = ticket;
+		i++;
+	}
+	
+	public void print() {
+		for(int j = 0; j < i; j++) {
+			System.out.printf("%-10s%-9s%-5d%-3d%d\n", t[j].origin, t[j].destination, t[j].price, t[j].quantity, (t[j].price*t[j].quantity));
+		}
 
+	}
 }
